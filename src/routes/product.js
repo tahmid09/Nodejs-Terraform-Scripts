1bdc84aca740cff069b1199fcd99346c37d936cc
Tahmid Hasan <hasantahmid09aece@gmail.com>
2023-12-04 09:51:51 +0600

    first commit

--- src/routes/product.js
+++ src/routes/product.js
@@ -28,10 +28,61 @@ router.get('/', authenticateToken, async (req, res) => {
 
 // Create a new product (requires authentication)
 router.post('/', authenticateToken, async (req, res) => {
-  // ... existing code ...
+  const { name, price, description } = req.body;
+
+  try {
+    const newProduct = new Product({
+      name,
+      price,
+      description,
+    });
+
+    const savedProduct = await newProduct.save();
+
+    res.status(201).json(savedProduct);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
 
-// Other CRUD routes go here...
+// Update a product
+router.put('/:id', authenticateToken, async (req, res) => {
+  const { id } = req.params;
+  const { name, price, description } = req.body;
+
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(
+      id,
+      { name, price, description },
+      { new: true } // Return the updated document
+    );
+
+    if (!updatedProduct) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+
+    res.json(updatedProduct);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// Delete a product
+router.delete('/:id', authenticateToken, async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const deletedProduct = await Product.findByIdAndRemove(id);
+
+    if (!deletedProduct) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+
+    res.json({ message: 'Product deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 
 
